5115c54c5585f4ac5523d8756650b578fd475176
aaalfaro <andyalfaro91@yahoo.com>
2018-03-26 23:54:00 -0400

    Update NewTalk.php

--- Senior project/andy_pong/NewTalk.php
+++ Senior project/andy_pong/NewTalk.php
@@ -1,36 +1,24 @@
 <?php
 
-
 	$connection = mysqli_connect("localhost","andy","password","SmallTalk");
-
 	
-if(!$connection)
-	
-{
-
+	if(!$connection)
+	{
 		echo "Connection Error ".mysqli_connect_error();
 	}
-
-
+	//The name of the new table.
 	$Smalltalk = $_POST['presentation'];
-
-
+	
+	//Creates a table with the submitted name
 	$sql = "CREATE TABLE $Smalltalk(First VARCHAR(255) NOT NULL,Last VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL)";
 
-
+	//Outputs the appropriate message 
 	if ($connection->query($sql) === TRUE)
- 
 	{
-
     		echo "Table $Smalltalk created successfully";
-
 	}
- 
 	else
 	{
-
     		echo "Error creating table: " . $connection->error;
-
 	}
-
  ?>
