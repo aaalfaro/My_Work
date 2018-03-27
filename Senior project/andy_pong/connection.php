681e4c57e457198a6f4244e63cc4c4f450f8e97b
aaalfaro <andyalfaro91@yahoo.com>
2018-03-26 21:38:02 -0400

    Update connection.php

--- Senior project/andy_pong/connection.php
+++ Senior project/andy_pong/connection.php
@@ -3,36 +3,35 @@
 	$connection = mysqli_connect("localhost","andy","password","SmallTalk");
 
 	
-if(!$connection)
+	if(!$connection)
 	{
 
 		echo "Connection Error ".mysqli_connect_error();
 	
-}
+	}
 
 
 	else
 	{
 	
-	$email = urldecode($_POST['Var1']);
+		$email = urldecode($_POST['Var1']); //Student' email
 
-		$password = urldecode($_POST['Var2']);
+		$password = urldecode($_POST['Var2']); // Student's password
 
-	
-	$result = $connection ->query("SELECT email from Students where email='$email' and PW='$password'");
+		//Search for their credentials in the DB.
+		$result = $connection ->query("SELECT email from Students where email='$email' and PW='$password'");
 
 	
 	if( mysqli_num_rows($result) == 1){
 
-			echo "success";
-		
-}
+		echo "success";
+	}
 
-		else
-		{
-			echo "Incorrect email or password";
+	else
+	{
+		echo "Incorrect email or password";
 	
-		}
+	}
 
 	
 }
